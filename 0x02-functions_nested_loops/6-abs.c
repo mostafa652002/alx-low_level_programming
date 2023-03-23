71b433a9a12b14b101313732ca3d46b3478c4965
mostafa652002 <mosaid652002@gmail.com>
2023-03-23 08:24:50 -0700

    mostafa

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_abs - computes the absolute value of the integer argument
+ *
+ *@k: checks the number
+ *
+ * Return: 0
+ */
+
+int _abs(int k)
+{
+
+	if (k >= 0)
+	{
+		return (k);
+	}
+	else
+		return (k * -1);
+}
