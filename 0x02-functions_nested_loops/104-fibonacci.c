3d1941852f94741a64e6400b6d074a1cc1d73ce6
mostafa652002 <mosaid652002@gmail.com>
2023-03-23 08:20:12 -0700

    mostafa

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+unsigned long int Repeat, First = 1, Second = 2, Sum = 0;
+for (Repeat = 0 ; Repeat < 90 ; Repeat++)
+{
+printf("%lu, ", First);
+Sum = First + Second;
+First = Second;
+Second = Sum;
+if (Repeat == 89)
+printf("%lu\n", First);
+}
+return (0);
+}
