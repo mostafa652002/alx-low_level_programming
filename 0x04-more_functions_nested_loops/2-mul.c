6d0f644b175b32cd46db435844b8c8742343cb9b
mostafa652002 <mosaid652002@gmail.com>
2023-03-23 16:18:41 -0700

    mostafa

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - prints a multible of two ints
+ *
+ * @a: first int
+ * @b: second int
+ *
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	double c = a * b;
+
+	return (c);
+}
