735562f8d25ab73d24cb43f12d789465456b204d
mostafa652002 <mosaid652002@gmail.com>
2023-03-23 17:40:36 -0700

    mostafa

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,46 +1,28 @@
 #include "main.h"
 
 /**
- * print_number -  checks for checks for a digit (0 through 9).
- * @n: n -  Variable
+ * main - check the code for Holberton School students.
+ *
  * Return: Always 0.
  */
-void print_number(int n)
+int main(void)
 {
-	unsigned int z;
-	int m, b;
-
-	b = 10;
-
-	if (n < 10 && n >= 0)
-	{
-		_putchar (n + '0');
-	}
-	else if (n > -10 && n < 0)
-	{
-		n = n - 2 * n;
-		_putchar('-');
-		_putchar (n + '0');
-	}
-
-	else
-	{
-		if (n < 0)
-		{
-			n = n * -1;
-			_putchar ('-');
-		}
-		z = n;
-	while (z / b > 9)
-	{
-		b = b * 10;
-	}
-	while (b > 0)
-	{
-		m = z / b;
-		z = z % b;
-		_putchar (m + '0');
-		b = b / 10;
-	}
-	}
+	print_number(9);
+	_putchar('\n');
+	print_number(-2);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
+	print_number(-2147483648);
+	_putchar('\n');
+	print_number(2147483647);
+	_putchar('\n');
+	print_number(-0);
+	_putchar('\n');
+	return (0);
 }
+
